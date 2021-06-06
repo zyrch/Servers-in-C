fd67d2764489e34a700356a471c71d19355f5d5b
zyrch <rupanshu19475@iiitd.ac.in>
2021-06-06 19:28:52 +0530

    bugg fixed

--- concurrent/event-driven/select/server.c
+++ concurrent/event-driven/select/server.c
@@ -37,6 +37,7 @@ fd_state_t fd_state[MAXFDS];
 fd_status_t fd_status_R = (fd_status_t) {.want_read = true, .want_write = false};
 fd_status_t fd_status_W = (fd_status_t) {.want_read = false, .want_write = true};
 fd_status_t fd_status_RW = (fd_status_t) {.want_read = true, .want_write = true};
+fd_status_t fd_status_NRW = (fd_status_t) {.want_read = false, .want_write = false};
 
 void client_recv(int sockfd) {
   
@@ -60,11 +61,12 @@ void client_recv(int sockfd) {
     }
     perror("Error recv(): ");
     exit(-1);
-
   }else if (bytes_received == 0) {
+    puts("connection closed");
     // connection closed
     client_state->pState = IN_ACK;
-    client_state->fd_status = fd_status_R;
+    client_state->fd_status = fd_status_NRW;
+    close(sockfd);
     return;
   }
 
@@ -78,7 +80,7 @@ void client_recv(int sockfd) {
       client_state->pState = WAIT_FOR_MSG;
     }else {
       assert(client_state->write_buf_len < BUFF_SIZE);
-      client_state->write_buf[client_state->write_buf_len++] = buf[i];
+      client_state->write_buf[client_state->write_buf_len++] = buf[i] + 1;
       ready_to_send = true; 
     }
   }
@@ -102,13 +104,13 @@ void client_send(int sockfd) {
   int bytes_sent = send(sockfd, client_state->write_buf + client_state->write_buf_ptr, sendlen, 0);
   
   if (bytes_sent < 0) {
-    // TODO: why wasn't this done in previous versions
     if (errno == EAGAIN || errno == EWOULDBLOCK) {
-      // temporarily unavailabe
+      // temporarily unavailable
       client_state->fd_status = fd_status_R;
+      return;
     } else {
       perror("Error send:");
-      return;
+      exit(-1);
     }
   }
 
@@ -127,40 +129,67 @@ void client_send(int sockfd) {
 }
 
 
-void client_connect(int sockfd) {
+int client_connect(int sockfd) {
   
   assert(sockfd < MAXFDS);
 
-  fd_state_t *client_state = &fd_state[sockfd];
+  struct sockaddr_in peer_addr;
+  socklen_t peer_addr_size = sizeof(peer_addr);
+
+  int new_fd = accept(sockfd, (struct sockaddr *)&peer_addr, &peer_addr_size);
+  make_socket_non_blocking(new_fd);
+
+  assert(new_fd < MAXFDS);
+
+  if (new_fd < 0) {
+    if (errno == EAGAIN || errno == EWOULDBLOCK) {
+      printf("accept returns EAGAIN or EWOULDBLOCK\n");
+      return -1;
+    }else {
+      perror("Error: ");
+      exit(-1);
+    }
+  }
+
+  if (new_fd > CUR_MAXFDS) {
+    CUR_MAXFDS = new_fd;
+  }
+
+  print_peer_info(&peer_addr, peer_addr_size);
+  
+  fd_state_t *client_state = &fd_state[new_fd];
 
   client_state->pState = IN_ACK;
   client_state->write_buf_ptr = 0;
   client_state->write_buf_len = 0;
   client_state->fd_status = fd_status_W;
 
-  struct sockaddr_in peer_addr;
-  socklen_t peer_addr_size = sizeof(peer_addr);
+ 
+  client_state->write_buf[0] = '*';
+  client_state->write_buf_len = 1;
 
-  int new_fd = accept(sockfd, (struct sockaddr *)&peer_addr, &peer_addr_size);
+  return new_fd;
 
-  if (new_fd < 0) {
-    perror("Error: ");
-    return;
-  }
-  print_peer_info(&peer_addr, peer_addr_size);
 }
 
 void update_fds(fd_set *read, fd_set *write, int fd) {
+  
+  int cnt = 0;
   if (fd_state[fd].fd_status.want_read) {
+    ++cnt;
     FD_SET(fd, read);
   }else {
     FD_CLR(fd, read);
   }
 
   if (fd_state[fd].fd_status.want_write) {
+    ++cnt;
     FD_SET(fd, write);
   }else {
     FD_CLR(fd, write);
+  }
+  if (!cnt) {
+    close(fd);
   }
 }
 
@@ -169,6 +198,7 @@ int main(int argc, char **argv) {
   
   // listen_inet_socket exits if there is an error
   int sockfd = listen_inet_socket(8000);
+  make_socket_non_blocking(sockfd);
 
   printf("socket file descriptor : %d\n", sockfd);
 
@@ -181,6 +211,8 @@ int main(int argc, char **argv) {
   FD_SET(sockfd, &global_readfds);
   CUR_MAXFDS = sockfd;
 
+  fd_state[sockfd].fd_status.want_read = true;
+
   while (1) {
 
     // since select() changes the fd_set passed to it, copies
@@ -192,7 +224,7 @@ int main(int argc, char **argv) {
     // returns number of modified descriptors
     int modified = select(CUR_MAXFDS + 1, &local_readfds, &local_writefds, NULL, NULL);
 
-    if (modified == 0) {
+    if (modified < 0) {
       perror("Select: ");
       return -1;
     }
@@ -203,18 +235,21 @@ int main(int argc, char **argv) {
         --modified;
         if (fd == sockfd) {
           // New Client connect
-          client_connect(sockfd);
+          int new_fd = client_connect(fd);
+          if (new_fd < 0) continue;
+
+          update_fds(&global_readfds, &global_writefds, new_fd);
         }else {
-          // receive from client and switch set to write
-          client_recv(sockfd);
+          //receive from client and switch set to write
+          client_recv(fd);
+          update_fds(&global_readfds, &global_writefds, fd);
         }
-        update_fds(&global_readfds, &global_writefds, fd);
       }
 
       if (FD_ISSET(fd, &local_writefds)){
         --modified;
         // write to client and switch set
-        client_send(sockfd);
+        client_send(fd);
         update_fds(&global_readfds, &global_writefds, fd);
       }
     }
